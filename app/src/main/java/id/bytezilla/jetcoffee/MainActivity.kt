ab42cbf4f8d7b79b3466eda3b8376bd0109a5eee
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-21 20:34:18 +0700

    Implement Banner composable and update JetCoffeeApp
    This commit modifies `MainActivity.kt` to introduce the main UI structure:
    - Creates a `Banner` composable that displays a banner image (using `R.drawable.banner`) with `ContentScale.Crop` and overlays the `Search` component.
    - Updates `JetCoffeeApp` to act as the root container, utilizing a `Box` with `windowInsetsPadding` for safe drawing area handling.
    - Adds necessary imports for layout management, images, and resources.

--- app/src/main/java/id/bytezilla/jetcoffee/MainActivity.kt
+++ app/src/main/java/id/bytezilla/jetcoffee/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.bytezilla.jetcoffee.ui.components.Search
 import id.bytezilla.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +39,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Box(
+        modifier = Modifier.windowInsetsPadding(
+            WindowInsets.safeDrawing
+        )
+    ) {
+        Column {
+            Banner()
+        }
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
 }
 
 
